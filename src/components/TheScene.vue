e0b5dd581dce8634781a094dcf7e255f3c870484
henry.bobka <henry.bobka@gofore.com>
2023-08-15 13:18:51 +0200

    add real models + textures

--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { vue3dLoader } from 'vue-3d-loader';
 
 const windowHeight = computed(() => {
   return window.innerHeight;
 });
+
+function onLoad() {
+  // moveCam();
+}
+function moveCam() {  
+  requestAnimationFrame(moveCam);
+  cameraInitialPosition.value.x += 0.01;
+  console.log(cameraInitialPosition.value.x);
+}
+
+const kroneInitialPosition = ref({ x: 0, y: 1, z: 0 });
+const kroneInitialRotation = ref({ x: 0, y: 4.6, z: 0 });
+
+const tractorInitialPosition = ref({ x: -6, y: 1, z: -0.7 });
+const tractorInitialRotation = ref({ x: -1.6, y: 0, z: 14 });
+
+const cameraInitialPosition = ref({ x: 5, y: 5, z: 10 });
 </script>
 
 <template>
   <vue3dLoader
     :height="windowHeight"
-    :filePath="['models/harvester.obj']"
+    :filePath="['models/krone/bigPack1290HDPVC-CCI_tris.fbx', 'models/tractor/tractor.obj']"
+    :mtlPath="[null, 'models/tractor/tractor.mtl']"
+    :position="[kroneInitialPosition, tractorInitialPosition]"
+    :rotation="[kroneInitialRotation, tractorInitialRotation]"
+    :cameraPosition="cameraInitialPosition"
     :backgroundColor="0x222222"
-    :rotation="{ x: -1, y: 0, z: 0.2 }"
-    :position="{ x: 0, y: -200, z: -500 }"
+    @load="onLoad"
   ></vue3dLoader>
 </template>
