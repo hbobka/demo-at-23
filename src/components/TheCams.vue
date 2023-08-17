ba42d66a9244feebb9f806c078cb412461d9cdc5
henry.bobka <henry.bobka@gofore.com>
2023-08-17 12:04:54 +0200

    add second video

--- src/components/TheCams.vue
+++ src/components/TheCams.vue
@@ -3,37 +3,61 @@ import { ref } from 'vue';
 import VideoComponent from '@/components/ui/VideoComponent.vue';
 import ButtonComponent from '@/components/ui/ButtonComponent.vue';
 
-const isVideoVisible = ref(false);
-const isButtonVisible = ref(true);
-
-const onIsActive = (state: boolean) => {
-  isVideoVisible.value = state;
-  isButtonVisible.value = !state;
+const isVideo1Visible = ref(false);
+const isCamButton1Visible = ref(true);
+const onIs1Active = (state: boolean) => {
+  isVideo1Visible.value = state;
+  isCamButton1Visible.value = !state;
+};
+const onIsVideo1Shown = (state: boolean) => {
+  isVideo1Visible.value = state;
+  isCamButton1Visible.value = !state;
 };
 
-const onIsVideoShown = (state: boolean) => {
-  isVideoVisible.value = state;
-  isButtonVisible.value = !state;
+const isVideo2Visible = ref(false);
+const isCamButton2Visible = ref(true);
+const onIs2Active = (state: boolean) => {
+  isVideo2Visible.value = state;
+  isCamButton2Visible.value = !state;
+};
+const onIsVideo2Shown = (state: boolean) => {
+  isVideo2Visible.value = state;
+  isCamButton2Visible.value = !state;
 };
 </script>
 
 <template>
-  <div class="video-player">
+  <div class="video-player-1">
     <ButtonComponent
-      v-if="isButtonVisible"
+      v-if="isCamButton1Visible"
       icon="cam1_64x64"
       class="cam-button"
-      @is-active="onIsActive"
+      @is-active="onIs1Active"
     />
-    <VideoComponent v-if="isVideoVisible" class="video" @is-video-shown="onIsVideoShown" />
+    <VideoComponent v-if="isVideo1Visible" class="video" @is-video-shown="onIsVideo1Shown" />
+  </div>
+  <div class="video-player-2">
+    <ButtonComponent
+      v-if="isCamButton2Visible"
+      icon="cam2_64x64"
+      class="cam-button"
+      @is-active="onIs2Active"
+    />
+    <VideoComponent v-if="isVideo2Visible" class="video" @is-video-shown="onIsVideo2Shown" />
   </div>
 </template>
 
 <style scoped>
-.video-player {
+.video-player-1 {
   position: absolute;
   top: 50%;
   left: 1rem;
+  transform: translateY(-50%);
+}
+.video-player-2 {
+  position: absolute;
+  top: 50%;
+  right: 1rem;
   transform: translateY(-50%);
 }
 </style>
