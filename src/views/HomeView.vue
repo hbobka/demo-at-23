5ee34a183c636dacf5c531bd08e4f3e7146c13e0
henry.bobka <henry.bobka@gofore.com>
2023-08-18 10:20:15 +0200

    add bottom bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
 import TheScene from '@/components/TheScene.vue';
 import TheHeader from '@/components/TheHeader.vue';
 import TheCams from '@/components/TheCams.vue';
+import TheBottomBars from '@/components/TheBottomBars.vue';
 </script>
 
 <template>
   <TheHeader />
   <TheCams />
+  <TheBottomBars />
   <TheScene />
 </template>
