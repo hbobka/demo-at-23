1947c4392f01b1af015c8d1adbf39b2584aadb03
henry.bobka <henry.bobka@gofore.com>
2023-08-22 16:56:32 +0200

    add base panel component

---
+++ src/components/ThePanels.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import PanelComponent from '@/components/ui/PanelComponent.vue';
+
+const headingPressure = computed(() => 'Pressure');
+const headingDistance = computed(() => 'Distance');
+</script>
+
+<template>
+  <div class="panel-wrapper">
+    <PanelComponent :heading="headingPressure">
+      <p>hello 1</p>
+    </PanelComponent>
+    <PanelComponent :heading="headingDistance">
+      <p>hello 2</p>
+    </PanelComponent>
+  </div>
+</template>
+
+<style scoped>
+.panel-wrapper {
+  position: absolute;
+  top: 6rem;
+  right: 1rem;
+}
+</style>
---
+++ src/components/ui/PanelComponent.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps({
+  heading: {
+    type: String,
+    default: 'Panel'
+  }
+});
+
+const isPanelConentVisible = ref(false);
+const onPanelHeaderClick = () => {
+  isPanelConentVisible.value = !isPanelConentVisible.value;
+};
+</script>
+
+<template>
+  <div class="panel">
+    <div class="panel-header" @click="onPanelHeaderClick">
+      {{ heading }}
+    </div>
+    <div v-if="isPanelConentVisible" class="panel-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.panel {
+  background: var(--cci-black);
+  border-radius: 5px;
+  margin: 0.5rem;
+}
+
+.panel-header {
+  padding: 0.5rem 1rem;
+  color: var(--cci-white);
+}
+
+.panel-header:hover {
+  cursor: pointer;
+}
+
+.panel-content {
+  background: green;
+  width: 100%;
+}
+</style>
